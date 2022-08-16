faf1db969d7de9da3ea89715ead4f0ed7c20689b
Douglas Shibata <douglas.shibata11@gmail.com>
2022-08-16 13:27:44 -0300

    Container que ira exibir o resultado

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Alcool ou Gasolina',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.teal,
       ),
       home: HomePage(),
     );
@@ -35,9 +35,54 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Compensa",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 40,
+                  fontFamily: "Big Shoulders Display",
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(30),
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(60),
+                ),
+                child: TextButton(
+                  child: Text(
+                    "Calcular novamente",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontFamily: "Big Shoulders Display",
+                      fontSize: 25,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              )
+            ]),
+          ),
           Input(label: "Gasolina", controller: _gasController),
           Input(label: "Álcool", controller: _alcoolController),
           Container(
+            width: double.infinity,
             margin: EdgeInsets.all(30),
             height: 60,
             decoration: BoxDecoration(
